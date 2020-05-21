713bd609923541bcaf89abd22eb0b378536021cd
Jeena Chemparathy <jeena@Jeenas-MacBook-Pro-2.local>
2020-05-21 12:23:58 -0700

    Coding exercise

---
+++ variables/code.py
@@ -0,0 +1,13 @@
+# Create two variables, var1 and var2, both with the same value.
+var1 = 2
+var2 = var1
+
+print(var1)
+print(var2)
+
+# Create two variables, num1 and num2, which multiply together to give 16.
+num1 = 2
+num2 = 8
+result = num1 * num2
+
+print(result)
