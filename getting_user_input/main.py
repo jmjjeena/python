0389c90635a72e6fc4230b0fe0bd06d0324af87c
Jeena Chemparathy <jeena@Jeenas-MacBook-Pro-2.local>
2020-05-25 11:18:01 -0700

    Clean up

--- getting_user_input/main.py
+++ getting_user_input/main.py
@@ -2,13 +2,8 @@ name = input('Enter your name: ')
 # print(name)
 
 size_input = input('How big is your house (in square feet): ')
-# print(size_input)
 square_feet = int(size_input)
-square_meters = square_feet/10.8
-# print(square_meters)
-
-# print something nicer
+square_meters = (square_feet/10.8)
 #  this number of square feet  is this number of square meter
-print(f"{square_feet} square feet is {square_meters:.2f} square meters.")
-
 # :.2f --> format with 2 decimal places only
+print(f"{square_feet} square feet is {square_meters:.2f} square meters.")
