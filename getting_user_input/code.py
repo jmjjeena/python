1aa584e6c0d644c998c5d04e34d4726f1b077c2e
Jeena Chemparathy <jeena@Jeenas-MacBook-Pro-2.local>
2020-05-25 11:52:13 -0700

    Try to fix the issue

--- getting_user_input/code.py
+++
@@ -1,9 +0,0 @@
-name = input('Enter your name: ')
-# print(name)
-
-size_input = input('How big is your house (in square feet): ')
-square_feet = int(size_input)
-square_meters = (square_feet/10.8)
-#  this number of square feet  is this number of square meter
-# :.2f --> format with 2 decimal places only
-print(f"{square_feet} square feet is {square_meters:.2f} square meters.")
