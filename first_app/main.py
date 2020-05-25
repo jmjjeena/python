48472cfbfab520e1b6e452fa541a020da203b65a
Jeena Chemparathy <jeena@Jeenas-MacBook-Pro-2.local>
2020-05-25 12:41:22 -0700

    First App is done

---
+++ first_app/main.py
@@ -0,0 +1,15 @@
+# print years into months
+user_age = input("Enter your age: ")
+years = int(user_age)
+months = years * 12
+print(f"\nYour age, {years}, is equal to {months} months.")
+
+# print years into days
+age = int(input("Enter your age: "))
+days = age * 365
+print(f"\nYour age, {years}, is equal to {days} days.")
+
+# print years converted into seconds
+age_years = int(input("Tell me your age, please: "))
+seconds = age_years * (365*24*60*60)
+print(f"\nBatman says you age in seconds is {seconds}")
