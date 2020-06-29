8b61540269231513248ec97a6fe405185e16f88b
Jeena Chemparathy <jeena@Jeenas-MacBook-Pro-2.local>
2020-06-28 20:38:28 -0700

    Finish boolean

---
+++ booleans/main.py
@@ -0,0 +1,21 @@
+print(5 == 5)
+print(5 >= 4)
+print(10 != 10)
+
+'''
+    --> Comparison: ==, !=, >, <, >=, <=
+'''
+
+friends = ["Matt", "Jen"]
+abroad = ["Matt", "Jen"]
+friends_abroad = abroad
+
+
+print(friends == abroad)
+print(friends is abroad)
+print(abroad is friends_abroad)
+
+'''
+    --> 'is' - checks whether the the elements in the 2 lists are exact the same
+    --> use '==' instead of 'is'
+'''
